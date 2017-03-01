328eadd61d56bda3b0032c544aecefa9afe1db4d
Brian Pritt and Daniel Munger <brian+daniel@>
2017-02-28 17:04:37 -0800

    change default map view, add marker click info window.

--- src/DarkSky/wwwroot/js/index.js
+++ src/DarkSky/wwwroot/js/index.js
@@ -27,12 +27,39 @@ var initMap = function (latitude, longitude) {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 8,
         center: center,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
     });
     
     var marker = new google.maps.Marker({
         position: center,       
-        map: map
+        map: map,
+        title: 'Marker Title'
     });
+    var contentString = '<div id="content">' +
+            '<div id="siteNotice">' +
+            '</div>' +
+            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+            '<div id="bodyContent">' +
+            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'sandstone rock formation in the southern part of the ' +
+            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+            'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+            'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+            'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+            'Aboriginal people of the area. It has many springs, waterholes, ' +
+            'rock caves and ancient paintings. Uluru is listed as a World ' +
+            'Heritage Site.</p>' +
+            '.</p>' +
+            '</div>' +
+            '</div>';
+    var infoWindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    marker.addListener('click', function () {
+        infoWindow.open(map, marker);
+    });
+    
     function addMarker(feature)
     {
         var marker = new google.maps.Marker({
@@ -40,6 +67,7 @@ var initMap = function (latitude, longitude) {
             map: map
         })
     }
+
     var features = [
         {
             position: new google.maps.LatLng(45, -122)
